65279fc853c39c46051fc7e2179779c763e3b849
PrincePhil-prog <2425402798@live.gctu.edu.gh>
2026-05-31 02:35:37 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,41 @@
+# File: initials.py
+# Description: Prints stylized large initials PAT with surrounding periods.
+# Assignment Number: 1
+#
+# Name: Prince Philip Adjin Tetteh
+# STUDENT ID: 2425402798
+# Email: 2425402798@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Prince Philip Adjin Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints the small initials and stylized large initials for PAT."""
+    print()
 
 
+    print("...PAT")
 
-main()
+
+    print()
+
+
+    print("...PPPPPPPPPP...............AA.............TTTTTTTTTTTT.....")
+    print("...PPPPPPPPPPP.............AAAA............TTTTTTTTTTTT.....")
+    print("...PPPP....PPPP...........AA..AA...............TTTT.........")
+    print("...PPPP....PPPP..........AA....AA..............TTTT.........")
+    print("...PPPPPPPPPPP..........AAA....AAA.............TTTT.........")
+    print("...PPPPPPPPPP..........AAAAAAAAAAAA............TTTT.........")
+    print("...PPPP................AAA......AAA............TTTT.........")
+    print("...PPPP................AAA......AAA............TTTT.........")
+    print("...PPPP...........**...AAA......AAA...**.......TTTT.......**")
+    print("...PPPP...........**...AAA......AAA...**.......TTTT.......**")
+
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
